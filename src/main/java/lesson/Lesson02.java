2a75c6b59abaff384401b83be385622e400b2201
Jakub Drozdz <jakubekso@outlook.com>
2023-10-05 16:11:53 +0200

    Lesson 02 and 03

---
+++ src/main/java/lesson/Lesson02.java
@@ -0,0 +1,20 @@
+package lesson;
+
+public class Lesson02 {
+    public static void main(String[] args) {
+        for (int x : solution(new int[]{3,8,9,7,6}, 3)) {
+            System.out.print(x + " ");
+        }
+    }
+    public static int[] solution(int[] A, int K) {
+        int[] tmpArr = new int[A.length];
+        for (int i = 0; i < A.length; i++) {
+            if(i+K < A.length){
+                tmpArr[i+K] = A[i];
+            }else{
+                tmpArr[(i+K)% A.length] = A[i];
+            }
+        }
+        return tmpArr;
+    }
+}
---
+++ src/main/java/lesson/Lesson03.java
@@ -0,0 +1,14 @@
+package lesson;
+
+public class Lesson03 {
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{9,3,9,3,9,7,9}));
+    }
+    public static int solution(int[] A) {
+        int result = 0;
+        for (int x : A) {
+            result ^= x;
+        }
+        return result;
+    }
+}
